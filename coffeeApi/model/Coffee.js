1c98a6b86b16686377e25abc1ebb323ffce95015
= <chayapam@gmail.com>
2021-02-20 16:23:25 +0700

    added coffees routes with mongodb database intergration

--- coffeeApi/model/Coffee.js
+++ coffeeApi/model/Coffee.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
-const coffeeSchema = new mongoose.Schema({
-     
+const coffeeSchema = new Schema({
+    name: String,
+    des: String,
+    price: Number
 })
 
-module.exports = mongoose.model('Coffee', coffeeSchema)
+const CoffeeModel = mongoose.model('Coffee', coffeeSchema)
+
+module.exports =  CoffeeModel
--- coffeeApi/routes/coffees.js
+++ coffeeApi/routes/coffees.js
@@ -1,17 +1,26 @@
-
 const express = require('express')
+const app = express()
 const router = express.Router()
+const mongoose = require('mongoose')
+const Coffee = require('../model/Coffee')
 
-app.get('/coffees',(req, res) => {
-    res.json(coffees)
+router.get('/',async (req, res) => {
+    const result = await Coffee.find()
+    res.json(result)
 })
 
-app.get('/coffees/:id',(req, res) => {
+router.get('/:id',(req, res) => {
     //get param
     const { id } = req.params
     const result = coffees.find( item => item.coffees.id == id)
     res.json(result)
-
 })
 
-router.exports
+router.post('/', async (req, res) => {
+    const payload = req.body
+    const coffee = new Coffee(payload)
+    await coffee.save()
+    res.status(201).end()
+})
+
+module.exports = router
