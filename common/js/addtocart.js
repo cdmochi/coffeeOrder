b2a77f9422c3729b1a10e95545ccb747a19f7488
= <chayapam@gmail.com>
2021-03-17 14:58:25 +0700

    add Coffee class to addtocart.js

--- common/js/addtocart.js
+++ common/js/addtocart.js
@@ -3,3 +3,15 @@ $(document).ready(
         
     }
 )
+
+
+//Models
+class Coffee {
+    constructor(id, name, description, price, imgSrc) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imgSrc = imgSrc;
+    }
+};
